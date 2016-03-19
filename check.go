8131f46b2f51ce7848af7ace2780c7d6cb69c486
Sergey Romanov <xxsmotur@gmail.com>
2016-03-19 16:56:00 +0500

    Draft implementation of CheckHTTP

--- check.go
+++ check.go
@@ -3,6 +3,8 @@ package heartbeat
 import (
    "sync"
    "net/http"
+   "errors"
+   "encoding/json"
 )
 
 type Check struct {
@@ -39,8 +41,15 @@ func (check*Check) Run() {
 	check.run()
 }
 
-func (check*Check) CheckHTTP() {
+func (check*Check) CheckHTTP()([]byte, error) {
+	for _, value := range check.httpCheck {
+		resp, err := check.checkItem(value.target)
+		if err != nil {
+			value.status = "unhealthy"
+		}
+	}
 
+	return json.Marshal(check.httpCheck)
 }
 
 func (check*Check) run(){
@@ -48,14 +57,24 @@ func (check*Check) run(){
 
 	go func(){
 		for _, value := range check.httpCheck {
-			resp, err := http.Get(value.target)
+			_, err := check.checkItem(value.target)
 			if err != nil {
 
 			}
-
-			if resp.StatusCode > 400 {
-				value.status = "unhealthy"
-			}
+			
 		}
 	}()
+}
+
+func (check*Check) checkItem(target string)(*http.Response, error) {
+	resp, err := http.Get(target)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode > 400 {
+		return resp, errors.New("Unhealthy")
+	}
+
+	return resp, nil
 }
