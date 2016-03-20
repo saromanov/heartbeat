203a0bc82eb86895c2e382f96a70ada670e87f35
Sergey Romanov <xxsmotur@gmail.com>
2016-03-20 23:59:27 +0500

    Add comment

--- check.go
+++ check.go
@@ -41,6 +41,7 @@ func (check*Check) Run() {
 	check.run()
 }
 
+// CheckHTTP method for checking health by http
 func (check*Check) CheckHTTP()([]byte, error) {
 	for _, value := range check.httpCheck {
 		resp, err := check.checkItem(value.target)
