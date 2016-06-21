a0d18b58bb5576e1c6535137e1ed21a8ae1d5a35
Sergey <xxsmotur@gmail.com>
2016-06-22 04:05:02 +0500

    Add json annotations

--- httpReport.go
+++ httpReport.go
@@ -4,12 +4,12 @@ import (
 	"time"
 )
 type HTTPReport struct {
-
+	items []HTTPItem `json:"items"`
 }
 
 type HTTPItem struct {
-	Name  string
-	Status  string
-	Time   *time.Time
-	Tags   []string
+	Name  string `json:"name"`
+	Status  string `json:"status"`
+	Time   *time.Time `json:"time"`
+	Tags   []string `json:"tags"`
 }
