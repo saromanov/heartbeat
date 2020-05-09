6d57ceacf0dc379356fc8ec7e10f35a7a8095236
Sergey <xxsmotur@gmail.com>
2020-05-09 23:59:32 +0500

    Add example

--- examples/main.go
+++ examples/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/saromanov/heartbeat"
+)
+
+func main() {
+	h := heartbeat.New()
+	h.AddHTTPCheck("some", "https://github.com")
+	r, err := h.CheckHTTP()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(r)
+}
