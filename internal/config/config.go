0f90e344c2c906fc1150acf96caeae5f4b54831c
Sergey <xxsmotur@gmail.com>
2020-06-26 23:59:11 +0500

    Adding load by config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,24 @@
 package config
 
+import (
+	"time"
+
+	"github.com/saromanov/cowrow"
+)
+
 // Config defines configuration for the app
 type Config struct {
-	Address string  `yaml:"address"`
-	Checks  []Check `yaml:"checks"`
+	Duration time.Duration `yaml:"duration"`
+	Address  string        `yaml:"address"`
+	Checks   []Check       `yaml:"checks"`
 }
 
 // Check defines http check
 type Check struct {
 	Name string `yaml:"name"`
 	URL  string `yaml:"url"`
+}
+
+func Unmarshal(path string) (*Config, error) {
+	cowrow.LoadByPath(path)
 }
