177bee722aa416163da3da1b67b01a81a6ee2edc
Sergey <xxsmotur@gmail.com>
2020-08-08 19:48:36 +0500

    Implementation of writer to the file

--- internal/core/writer/file/file.go
+++ internal/core/writer/file/file.go
@@ -1,1 +1,34 @@
 package file
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/saromanov/heartbeat/internal/core/writer"
+)
+
+// File defines file writer
+type File struct {
+	file *os.File
+}
+
+// New provides initialization of the file writer
+func New(fileName string) (writer.Writer, error) {
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return &File{
+		file: f,
+	}, nil
+}
+
+// Write provides writing of teh data to file
+func (f *File) Write(data []byte) error {
+	_, err := f.file.Write(data)
+	if err != nil {
+		return fmt.Errorf("unable to write data: %v", err)
+	}
+	return nil
+}
--- internal/core/writer/writer.go
+++ internal/core/writer/writer.go
@@ -2,4 +2,5 @@ package writer
 
 // Writer provides writing of the data to sources
 type Writer interface {
+	Write([]byte) error
 }
