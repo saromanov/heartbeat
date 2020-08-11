4732be15280533631b08e819ab748d7dc599b102
Sergey <xxsmotur@gmail.com>
2020-08-11 21:46:52 +0500

    Add method for load config

--- cmd/heartbeat/main.go
+++ cmd/heartbeat/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/saromanov/heartbeat/internal/config"
 	"github.com/saromanov/heartbeat/internal/server"
 )
 
+func loadConfig(path string) (*config.Config, error) {
+	cfg, err := config.Unmarshal(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to load config: %v", err)
+	}
+	return cfg, err
+}
 func main() {
-	server.Run(config.Default())
+	path := flag.String("config-path", "", "path to config path")
+	flag.Parse()
+
+	conf := config.Default()
+	if *path != "" {
+		confTmp, err := loadConfig(*path)
+		if err != nil {
+			panic(err)
+		}
+		conf = confTmp
+	}
+	server.Run(conf)
 }
